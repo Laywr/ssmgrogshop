3d90d962d7f1612e048a7ade25db2a3a91392aa9
lay <1824611023@qq.com>
2021-04-20 11:10:51 +0800

    数据展示页面代码优化

--- src/main/java/com/gx/web/DateView.java
+++ src/main/java/com/gx/web/DateView.java
@@ -1,29 +1,17 @@
 package com.gx.web;
 
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.gx.po.StayRegisterPo;
+import com.gx.service.StayRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.gx.page.Page;
-import com.gx.po.AttributePo;
-import com.gx.po.CommodityPo;
-import com.gx.po.StayRegisterPo;
-import com.gx.po.UserPo;
-import com.gx.service.StayRegisterService;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 @Controller
 @RequestMapping("/DateView")
@@ -34,13 +22,16 @@ public class DateView {
 
 	@RequestMapping("/tolist")
 	public ModelAndView list(){
-		ModelAndView mv=null;
+		ModelAndView mv=new ModelAndView("/dateview/shili");
 		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);         //获取年
-		int month = cal.get(Calendar.MONTH) + 1;   //获取月
+		//获取年
+		int year = cal.get(Calendar.YEAR);
+		//获取月
+		int month = cal.get(Calendar.MONTH) + 1;
 		List<Integer> monthList = new ArrayList<>();
 		List<Integer> yearList = new ArrayList<>();
 		List<Timestamp> timestampList = new ArrayList<>();
+		//添加前一年的月份
 		for (int i = 0; i < 13; i++) {
 			if( i == 0){
 				monthList.add(month);
@@ -49,6 +40,7 @@ public class DateView {
 			}
 		}
 		List<String> monthStringList = new ArrayList<>();
+		//添加前一年的年份
 		for(Integer months : monthList){
 			if(months > 0){
 				yearList.add(year);
@@ -58,9 +50,7 @@ public class DateView {
 				monthStringList.add(String.valueOf(months+12));
 			}
 		}
-		for (String s : monthStringList) {
-			System.out.println(s);
-		}
+		//将个位数的月份前加0
 		String[] strings = monthStringList.toArray(new String[monthStringList.size()]);
 		for (int i = 0; i < strings.length; i++) {
 			if(strings[i].length() == 1){
@@ -70,17 +60,18 @@ public class DateView {
 		monthStringList = Arrays.asList(strings);
 		List<List<StayRegisterPo>> stayRegisterPoList = new ArrayList<>();
 		for (int i = 0; i < 13; i++) {
-			System.out.println(yearList.get(i)+"-"+monthStringList.get(i)+"-"+"01"+" 00:00:00");
+			//格式化各月份
 			timestampList.add(Timestamp.valueOf(yearList.get(i)+"-"+monthStringList.get(i)+"-"+"01"+" 00:00:00"));
 			if(i != 0){
+				//查询各月份的销售额
 				stayRegisterPoList.add(stayRegisterService.selectShuJuTongJi(timestampList.get(i),timestampList.get(i-1)));
 			}
 		}
-		mv=new ModelAndView("/dateview/shili");
 		Integer count = 1;
 		for(List<StayRegisterPo> stayRegisterPos:stayRegisterPoList) {
 			double individualSum = 0D;
 			double teamSum = 0D;
+			//分别添加个人和团体的销售额到模型视图中
 			for (int i = 0; i < stayRegisterPos.size(); i++) {
 				if (stayRegisterPos.get(i).getReceiveTargetID() == 2) {
 					individualSum += stayRegisterPos.get(i).getSumConst();
@@ -91,6 +82,7 @@ public class DateView {
 			mv.addObject("sZongFeiYong" + (count++), individualSum);
 			mv.addObject("tZongFeiYong" + count, teamSum);
 		}
+		//添加日期到模型视图中
 		for (int i = 1; i < 13; i++) {
 			mv.addObject("year"+i,yearList.get(i)+"年"+monthStringList.get(i)+"月");
 		}
